fd6d38d09ce4d8d1f8f94db30e29c11fb7be0c16
Shobhit <shobhittrivedi1111@gmail.com>
2024-04-11 10:57:25 +0530

    city changed

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("shobhit trivedi");
-        System.out.println("Barabanki");
+        System.out.println("lucknow");
         System.out.println("pincode: "+ 225124);
     }
 }
