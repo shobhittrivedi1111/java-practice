2f766cb60e956f85e2417d4b50f747102cc2246a
Shobhit <shobhittrivedi1111@gmail.com>
2024-04-11 10:34:04 +0530

    pincode added

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("shobhit trivedi");
         System.out.println("Barabanki");
+        System.out.println("pincode: "+ 225124);
     }
 }
